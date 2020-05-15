60f44cd3d6067143184601a99eafda789532371b
Lars Moelleken <lars.moelleken@vdmg-it.com>
2020-05-16 00:24:38 +0200

    [+]: features + fixes
    -> added GenerateApi->hideTheFunctionIndex
    
    -> fix "href" usage
    
    -> use more templates

--- src/voku/PhpReadmeHelper/GenerateApi.php
+++ src/voku/PhpReadmeHelper/GenerateApi.php
@@ -3,6 +3,7 @@
 namespace voku\PhpReadmeHelper;
 
 use voku\PhpReadmeHelper\Template\TemplateFormatter;
+use voku\SimplePhpParser\Model\PHPMethod;
 use voku\SimplePhpParser\Parsers\PhpCodeParser;
 
 class GenerateApi
@@ -25,18 +26,23 @@ class GenerateApi
     public $skipMethodsWithLeadingUnderscore = true;
 
     /**
+     * @var bool
+     */
+    public $hideTheFunctionIndex = false;
+
+    /**
      * @var string
      */
     public $templateMethod = <<<RAW
 ## %name%
-<a href="#class-methods">↑</a>
+<a href="#voku-php-readme-class-methods">↑</a>
 %description%
 
 **Parameters:**
 %params%
 
 **Return:**
-- `%return%`
+%return%
 
 --------
 
@@ -57,6 +63,13 @@ RAW;
 RAW;
 
     /**
+     * @var string
+     */
+    public $templateMethodReturn = <<<RAW
+- `%return%`
+RAW;
+
+    /**
      * 'public' || 'protected' || 'private'
      *
      * @var string[]
@@ -64,13 +77,14 @@ RAW;
     public $access = ['public'];
 
     /**
-     * @param string        $codePath
-     * @param string        $baseDocFilePath
-     * @param string[]|null $useClasses
+     * This method can generate API documentation with types from php code into your README file.
+     *
+     * @param string        $codePath        <p>Path to a file or a directory.</p>
+     * @param string        $baseDocFilePath <p>Path to your base file for the README.</p>
+     * @param string[]|null $useClasses      <p>If used, you can specify to classes, you will use.</p>
      *
      * @return string
-     *
-     * @noinspection NestedTernaryOperatorInspection
+     *                <p>Return a string with the generated README.</p>
      */
     public function generate(
         string $codePath,
@@ -129,100 +143,152 @@ RAW;
                     continue;
                 }
 
-                $methodIndexTemplate = new TemplateFormatter($this->templateIndexLink);
+                $paramsInfo = $this->getMethodParams($method);
+
+                $returnInto = $this->getMethodReturn($method);
+
+                $methodWithType = $method->name . '(' . \trim(\implode(', ', $paramsInfo['paramsTypes'])) . '): ' . $method->returnTypeFromPhpDoc;
+
+                // method --------------------------------------------------------------
 
                 $methodTemplate = new TemplateFormatter($this->templateMethod);
 
-                // -- params
-                $params = [];
-                $paramsTypes = [];
-                foreach ($method->parameters as $param) {
-                    $paramsTemplate = new TemplateFormatter($this->templateMethodParam);
-                    $paramsTemplate->set(
-                        'param',
-                        (
-                            $param->typeFromPhpDocPslam
-                                ?: $param->typeFromPhpDoc
-                                ?: $param->type
-                                ?: ($this->todoModus ? 'TODO: __not_detected__' : '')
-                        ) . (
-                            GenerateStringHelper::str_replace_beginning($param->typeMaybeWithComment, $param->typeFromPhpDoc, '')
-                                ?: ' ' . '$' . $param->name
-                        )
-                    );
-                    $params[] = $paramsTemplate->format();
-
-                    $paramsTypes[] = (
-                        $param->typeFromPhpDocSimple
-                            ?: $param->typeFromPhpDoc
-                            ?: $param->type
-                            ?: ($this->todoModus ? 'TODO: __not_detected__' : '')
-                    ) . ' ' . '$' . $param->name;
-                }
-
-                if (\count($params) !== 0) {
-                    $methodTemplate->set('params', \implode("\n", $params));
+                if (\count($paramsInfo['params']) > 0) {
+                    $methodTemplate->set('params', \implode("\n", $paramsInfo['params']));
                 } else {
                     $methodTemplate->set('params', '__nothing__');
                 }
 
-                // -- return
-
-                $methodWithType = $method->name . '(' . \trim(\implode(', ', $paramsTypes)) . '): ' . $method->returnTypeFromPhpDoc;
-
-                $description = \trim($method->summary . "\n\n" . $method->description);
-
+                $methodTemplate->set('return', $returnInto);
                 $methodTemplate->set('name', $methodWithType);
-                $methodTemplate->set('description', $description);
-                $methodTemplate->set(
-                    'return',
-                    $method->returnTypeMaybeWithComment
-                        ?: $method->returnTypeFromPhpDocPslam
-                        ?: $method->returnType
-                        ?: ($this->todoModus ? 'TODO: __not_detected__' : '')
-                );
-
-                $methodIndexTemplate->set('title', $method->name);
-                $methodIndexTemplate->set('href', '#' . GenerateStringHelper::css_identifier($methodWithType));
+                $methodTemplate->set('description', \trim($method->summary . "\n\n" . $method->description));
 
                 $functionsDocumentation[$method->name] = $methodTemplate->format();
-                $functionsIndex[$method->name] = $methodIndexTemplate->format();
+
+                // index --------------------------------------------------------------
+
+                if ($this->hideTheFunctionIndex !== true) {
+                    $methodIndexTemplate = new TemplateFormatter($this->templateIndexLink);
+                    $methodIndexTemplate->set('title', $method->name);
+                    $methodIndexTemplate->set('href', '#' . GenerateStringHelper::css_identifier($methodWithType));
+
+                    $functionsIndex[$method->name] = $methodIndexTemplate->format();
+                }
             }
 
-            \ksort($functionsDocumentation);
-            $functionsDocumentation = \array_values($functionsDocumentation);
+            \ksort($functionsDocumentation, \SORT_NATURAL);
+            \ksort($functionsIndex, \SORT_NATURAL);
 
-            \ksort($functionsIndex);
-
-            // -------------------------------------
+            // --------------------------------------------------------------
 
             $documentTemplate->set($functionListHelper, \implode("\n", $functionsDocumentation));
 
-            $indexLastChar = null;
-            $indexStrResult = '';
-            $counterTmp = 0;
-            foreach ($functionsIndex as $_index => $_template) {
-                ++$counterTmp;
-
-                if ($counterTmp === 1) {
-                    $indexStrResult .= '<tr>';
-                }
-
-                $indexStrResult .= '<td>' . \sprintf("%s\n", $_template) . '</td>';
-
-                if ($counterTmp === 4) {
-                    $counterTmp = 0;
-                    $indexStrResult .= '</tr>';
-                }
-            }
-            if ($counterTmp > 0) {
-                $indexStrResult .= '</tr>';
-            }
-            $indexStrResult = '<table>' . $indexStrResult . '</table>';
-
+            $indexStrResult = $this->getIndexHtmlTable($functionsIndex);
             $documentTemplate->set($functionIndexHelper, $indexStrResult);
         }
 
-        return $documentTemplate->format();
+        return '[//]: # (AUTO-GENERATED BY "PHP README Helper": base file -> ' . \str_replace(\getcwd() . DIRECTORY_SEPARATOR, '', $baseDocFilePath) . ')' . "\n" . $documentTemplate->format();
+    }
+
+    /**
+     * @param \voku\SimplePhpParser\Model\PHPMethod $method
+     *
+     * @return string[][]
+     *
+     * @psalm-return array{$params: string[], paramsTypes: string[]}
+     */
+    private function getMethodParams(PHPMethod $method): array
+    {
+        // init
+        $params = [];
+        $paramsTypes = [];
+
+        foreach ($method->parameters as $param) {
+            $paramsTemplate = new TemplateFormatter($this->templateMethodParam);
+            /** @noinspection NestedTernaryOperatorInspection */
+            $paramsTemplate->set(
+                'param',
+                (
+                    $param->typeFromPhpDocPslam
+                        ?: $param->typeFromPhpDoc
+                        ?: $param->type
+                        ?: ($this->todoModus ? 'TODO: __not_detected__' : '')
+                )
+                .
+                (
+                    GenerateStringHelper::str_replace_beginning(
+                        $param->typeMaybeWithComment,
+                        $param->typeFromPhpDoc,
+                        ''
+                    ) ?: ' ' . '$' . $param->name
+                )
+            );
+
+            $params[] = $paramsTemplate->format();
+
+            /** @noinspection NestedTernaryOperatorInspection */
+            $paramsTypes[] = (
+                $param->typeFromPhpDocSimple
+                     ?: $param->typeFromPhpDoc
+                     ?: $param->type
+                     ?: ($this->todoModus ? 'TODO: __not_detected__' : '')
+            ) . ' ' . '$' . $param->name;
+        }
+
+        return ['params' => $params, 'paramsTypes' => $paramsTypes];
+    }
+
+    /**
+     * @param string[] $functionsIndex
+     *
+     * @return string
+     *
+     * @psalm-param array<string, string> $functionsIndex
+     */
+    private function getIndexHtmlTable(array $functionsIndex): string
+    {
+        // init
+        $indexLastChar = null;
+        $indexStrResult = '';
+        $counterTmp = 0;
+
+        foreach ($functionsIndex as $_index => $_template) {
+            ++$counterTmp;
+
+            if ($counterTmp === 1) {
+                $indexStrResult .= '<tr>';
+            }
+
+            $indexStrResult .= '<td>' . \sprintf("%s\n", $_template) . '</td>';
+
+            if ($counterTmp === 4) {
+                $counterTmp = 0;
+                $indexStrResult .= '</tr>';
+            }
+        }
+
+        if ($counterTmp > 0) {
+            $indexStrResult .= '</tr>';
+
+            $indexStrResult = '<table>' . $indexStrResult . '</table>';
+        }
+
+        return '<p id="voku-php-readme-class-methods"></p>' . $indexStrResult;
+    }
+
+    private function getMethodReturn(PHPMethod $method): string
+    {
+        $returnTemplate = new TemplateFormatter($this->templateMethodReturn);
+
+        /** @noinspection NestedTernaryOperatorInspection */
+        $returnTemplate->set(
+            'return',
+            $method->returnTypeMaybeWithComment
+                ?: $method->returnTypeFromPhpDocPslam
+                ?: $method->returnType
+                ?: ($this->todoModus ? 'TODO: __not_detected__' : '')
+        );
+
+        return $returnTemplate->format();
     }
 }
